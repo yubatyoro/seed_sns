4edf1fc9f10edc6ab5c7de28e3d11d64c556a586
阿部達也 <wakuwaku050677@icloud.com>
2018-01-08 15:20:18 +0800

    ロゴについているリンクをindex.htmlからindex.phpに修正

--- join/thanks.php
+++ join/thanks.php
@@ -28,7 +28,7 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="index.html"><span class="strong-title"><i class="fa fa-twitter-square"></i> Seed SNS</span></a>
+              <a class="navbar-brand" href="index.php"><span class="strong-title"><i class="fa fa-twitter-square"></i> Seed SNS</span></a>
           </div>
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
